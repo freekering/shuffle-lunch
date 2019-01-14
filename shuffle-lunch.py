9776bb566e553a213c09ab04f5865b150cd199fc
kering <freekering@gmail.com>
2019-01-14 18:17:36 +0900

    Open mpim channels for selected users and post message.

--- shuffle-lunch.py
+++ shuffle-lunch.py
@@ -1,9 +1,11 @@
+import datetime
 import random
 from slackclient import SlackClient
 
-slack_api_token = ''
-random_seed = 0
 chunk_size = 6
+slack_api_token = '' # bot user token required
+date = datetime.datetime.now().strftime('%Y-%m-%d')
+random_seed = date.replace('-', '')
 exclusion_emoji = None
 
 slack = SlackClient(slack_api_token)
@@ -31,7 +33,29 @@ for k in range(chunk_count):
     if k >= chunk_count - (chunk_size - remainder):
         size -= 1
     selected_users = users[start:(start + size)]
-    for u in selected_users:
-        print(u['real_name'])
+
+    response = slack.api_call(
+        'conversations.open',
+        users=list(map(lambda user: user['id'], selected_users))
+    )
+
+    if response['ok']:
+        response = slack.api_call(
+            'chat.postMessage',
+            channel=response['channel']['id'],
+            text=f':tada: {date} 셔플 런치 조가 생성되었습니다. 조장은 `{selected_users[0]["real_name"]}`님 입니다! :tada:'\
+                  '\n(조장이 불가피한 사유로 불참 시 남은 분들 중에 선출해주세요)',
+            as_user=True
+        )
+        if response['ok']:
+            print('채널 생성 성공:')
+        else:
+            print('채널 생성 실패:')
+    else:
+        print('채널 생성 실패:')
+
+    for user in selected_users:
+        print(user['real_name'])
     print()
+
     start += size
