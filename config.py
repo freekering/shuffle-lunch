757e3e3755a1c0c536c06a21aa4be5b10cdde826
kering <freekering@gmail.com>
2019-01-16 17:30:18 +0900

    Add `preannounce.py`.

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ SLACK_API_TOKEN = '' # bot user token required
 NOTICE_CHANNEL = '#announcement'
 DATE = 'yyyy-mm-dd'
 RANDOM_SEED = DATE.replace('-', '')
-EXCLUSION_STATUS_TEXT = '셔런불'
+EXCLUSION_STATUS_TEXT = '셔플런치:x:'
 USER_SPECIFIC_NOTICE_TEMPLATES = {
 }
+START_TIME_FOR_PREANNOUNCE = '내일 오후 2시 30분'
---
+++ preannounce.py
@@ -0,0 +1,12 @@
+import config
+from slackclient import SlackClient
+
+if __name__ == "__main__":
+    slack = SlackClient(config.SLACK_API_TOKEN)
+
+    slack.api_call(
+        'chat.postMessage',
+        channel=config.NOTICE_CHANNEL,
+        text=f'{config.START_TIME_FOR_PREANNOUNCE} {config.DATE} 셔플 런치 추첨이 시작됩니다.'\
+             f'\n휴가 등의 불가피한 이유로 참석이 어려우신 분은 번거롭더라도 그 전까지 슬랙 status text를 `{config.EXCLUSION_STATUS_TEXT}`로 설정해주시면 추첨에서 자동으로 제외됩니다. :thanks:'
+    )
